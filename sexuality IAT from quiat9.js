0fc61a4b306008f09a11948bc13ce7c703908697
masciacca <masciacca1@gmail.com>
2025-08-22 14:24:33 -0400

    sexuality IAT from quiat9.js

--- sexuality IAT from quiat9.js
+++ sexuality IAT from quiat9.js
@@ -1,5 +1,5 @@
 
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/masciacca/IAT-Sexuality-Spanish-v2@main/v2.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/masciacca/IAT-Spanish/quiat9_spanish%20language%20version.js'], function(APIConstructor, iatExtension){
 	var API = new APIConstructor();
 
 	var posWords = API.shuffle([
